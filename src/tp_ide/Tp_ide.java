45d790f37323db9a663ad3520c59f6a49b93110f
mjothy <mariojothy@gmail.com>
2013-09-27 17:43:32 +0200

    Création d'histogramme en cours
    Signed-off-by: mjothy <mariojothy@gmail.com>

--- src/tp_ide/Tp_ide.java
+++ src/tp_ide/Tp_ide.java
@@ -1,25 +1,37 @@
 package tp_ide;
 
+import java.io.*;
+
 /**
  *
- * @author paulyves
+ * @author paulyves, mjothy
  */
-import javax.imageio.*;
-import java.awt.Image;
+
 
 public class Tp_ide {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        System.out.println("Hello World");
+        histo("C:\\Users\\Mario\\Desktop\\lena.pgm");
         
     }
     
-    public BufferedImage loadImage(){
-        BufferedImage image;
-        image = ImageIO.read(new File("lena.pmg"));
+    public static void histo(String path) throws IOException {
+        FileReader fr = new FileReader(new File(path));
+        BufferedReader file = new BufferedReader(fr);
+        String a = "0";
+        for(int i=0; i<4; i++){
+            file.readLine();
+        }
+        int[] histo = new int[255];
+        int b= file.read();
+        while(b!=-1){
+            histo[b] ++;
+            b = file.read();
+        }
+        
     }
 }
